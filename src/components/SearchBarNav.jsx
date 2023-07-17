6e7fce75cdba60c1f062858a2fe8bef8554a2666
Hassene-Belhaj <hassene.belhaj@gmail.com>
2023-07-17 22:44:56 +0200

    add search bar on focus modal display

--- src/components/SearchBarNav.jsx
+++ src/components/SearchBarNav.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 import { useState } from 'react'
-import { AiOutlineSearch } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineSearch } from 'react-icons/ai'
 import styled from 'styled-components'
 import ContextData, { useContextData } from '../Context/ContextData'
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+position: fixed;
+inset: 0;
+background : rgba(0,0,0,0.95);
+backdrop-filter: blur(.4rem);
+z-index: 6000;
+
+`
+const FlexContainer = styled.div`
+width: 100%;
+height: 50% ;
+
+`
+const SearchToggle = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+input{
+  position:relative ;
+  width: 300px;
+  height: 2rem;
+  border-radius:7px ;
+  border: none;
+  ::placeholder{
+  padding-left: 1rem;
+  }
+}
+`
+
+
+
 
 const SearchBar = styled.div`
 width: 25%;
@@ -22,6 +58,7 @@ input{
   background: ${({theme})=>theme.background};
   padding-left: 1.5rem;
   outline: none;
+  cursor: pointer;
 ::placeholder{
   padding-left: .3rem;
   color: ${({theme})=>theme.color};
@@ -35,26 +72,47 @@ margin-left:4rem;
 }
 `
 const SearchIcon = styled(AiOutlineSearch)`
-position: absolute;
-top: 50%;
-transform: translateY(-50%);
-left: 4px;
+position: ${({position})=>position} ;
+top: ${({top})=>top};
+transform: ${({transform})=>transform? 'translateY(-50%)' : null};
+left: ${({left})=>left};
 fill: ${({theme})=>theme.color};
 `
+
+
 
 const SearchBarNav = () => {
     const [val,setValue] = useState('')
     const [result,setResult] = useState()
+    const [search , setSearch] = useState(false)
+    const searchToggle = () => setSearch(!search) 
     const [data] = useContextData()
     console.log(val);
+    console.log(search);
 
 
 
-  return (
-    <SearchBar >
-    <input type="text" value={val} onChange={e=>setValue(e.target.value)} placeholder='Search products' />
-  <SearchIcon />
-</SearchBar>
+  return (   
+  <>
+  {search ?  
+  <Container>
+     <FlexContainer>
+       <AiOutlineClose style={{position:'absolute',top:'0',right:'0',margin:'1rem'}} onClick={searchToggle} color='#fff' />
+       <SearchToggle>
+          <input type="text" value={val} onChange={e=>setValue(e.target.value)} placeholder='Search products' onFocus={()=>setSearch(true)} />
+          <SearchIcon  />
+       </SearchToggle>
+     </FlexContainer>
+  </Container>  
+  :   
+  <SearchBar >
+      <input type="text" value={val} onChange={e=>setValue(e.target.value)} placeholder='Search products' onFocus={searchToggle} />
+       <SearchIcon position={'absolute'} top={'50%'} transform='true' left={'.4rem'} />
+  </SearchBar>}
+
+  
+
+  </>  
   )
 }
 
